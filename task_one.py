28a69104f3edbb23f4632a590d2d15a76b8d60d9
GrigoryTishkov <grisatiskov@gmail.com>
2025-04-29 10:20:55 +0300

    Upd task_one

--- task_one.py
+++ task_one.py
@@ -6,7 +6,7 @@ for i in range(n):
     left, right = i, i
 
     while left >= 0 and right < n and list1[left] == list1[right]:
-        current_len = right - left
+        current_len = right - left + 1
         if current_len > max_len:
             max_len = current_len
         left -= 1
@@ -14,12 +14,12 @@ for i in range(n):
 
     left, right = i, i + 1
     while left >= 0 and right < n and list1[left] == list1[right]:
-        current_len = right - left
+        current_len = right - left + 1
         if current_len > max_len:
             max_len = current_len
         left -= 1
         right += 1
-if max_len > 2:
-    max_len += 1
+if max_len == 1:
+    max_len = 0
 print(max_len)
 # 1 2 3 4 3 2 1
